07993b7b09e5284bd86235d364356f8a22ee8993
patoluvsoly <patipato742@gmail.com>
2026-06-23 13:00:06 +0300

    profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,6 +4,7 @@ const user = JSON.parse(localStorage.getItem('user'))
 
 <template>
     <h1>Profile</h1>
+    <v-avatar image="logo.png" size="49"></v-avatar>
     <p>Name: {{ user.firstname }} {{ user.lastname }}</p>
     <p>Email: {{ user.email }}</p>
     <p>Phone: {{ user.phone }}</p>
